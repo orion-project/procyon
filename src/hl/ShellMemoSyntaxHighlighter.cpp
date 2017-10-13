71a395aac9ef2cce109b38c754f5519a184886b6
Nikolay Chunosov <nikolay.chunosov@xored.com>
2017-10-13 10:29:44 +0700

    couple new styles for shell memo highlighter

--- src/hl/ShellMemoSyntaxHighlighter.cpp
+++ src/hl/ShellMemoSyntaxHighlighter.cpp
@@ -7,17 +7,25 @@
 #define STYLE_COMMENT 2
 #define STYLE_SEPARATOR 3
 #define STYLE_OUTPUT 4
+#define STYLE_EXCLAME 5
+#define STYLE_QUESTION 6
+#define STYLE_OPTION 7
 
 HighlightingStyleSet* getShellmemoHighlightingStyles()
 {
     static HighlightingStyleSet styles;
     if (styles.isEmpty())
     {
+        // Take color names from here:
+        // https://www.w3.org/TR/SVG/types.html#ColorKeywords
         styles.insert(STYLE_COMMAND, getTextCharFormat("darkBlue"));
         styles.insert(STYLE_HEADER, getTextCharFormat("black", "bold"));
         styles.insert(STYLE_COMMENT, getTextCharFormat("darkGreen", "italic"));
         styles.insert(STYLE_SEPARATOR, getTextCharFormat("darkGray"));
         styles.insert(STYLE_OUTPUT, getTextCharFormat("darkMagenta"));
+        styles.insert(STYLE_EXCLAME, getTextCharFormat("red"));
+        styles.insert(STYLE_QUESTION, getTextCharFormat("magenta"));
+        styles.insert(STYLE_OPTION, getTextCharFormat("darkSlateBlue"));
     }
     return &styles;
 }
@@ -27,9 +35,12 @@ ShellMemoSyntaxHighlighter::ShellMemoSyntaxHighlighter(QTextDocument *parent) : 
     auto styles = getShellmemoHighlightingStyles();
     rules.append(HighlightingRule("^\\s*\\$\\s+.*$", 0, styles->value(STYLE_COMMAND)));
     rules.append(HighlightingRule("^\\s*\\*\\s+.*$", 0, styles->value(STYLE_HEADER)));
+    rules.append(HighlightingRule("^\\s*\\!\\s+.*$", 0, styles->value(STYLE_EXCLAME)));
+    rules.append(HighlightingRule("^\\s*\\?\\s+.*$", 0, styles->value(STYLE_QUESTION)));
+    rules.append(HighlightingRule("^\\s*\\>\\s+.*$", 0, styles->value(STYLE_OUTPUT)));
     rules.append(HighlightingRule("^\\s*#.*$", 0, styles->value(STYLE_COMMENT)));
+    rules.append(HighlightingRule("^\\s*-{2}.*$", 0, styles->value(STYLE_OPTION)));
     rules.append(HighlightingRule("^\\s*-{3,}.*$", 0, styles->value(STYLE_SEPARATOR)));
-    rules.append(HighlightingRule("^\\s*>\\s+.*$", 0, styles->value(STYLE_OUTPUT)));
 }
 
 void ShellMemoSyntaxHighlighter::highlightBlock(const QString &text)
